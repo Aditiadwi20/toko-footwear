e8492c60f00a0348578a64ca5068bfec09f923df
Aditiadwi20 <xinerfin9206@gmail.com>
2026-06-12 21:15:21 +0700

    update 2

--- server.ts
+++ server.ts
@@ -62,15 +62,18 @@ async function getDbConnection() {
 
   try {
     pool = mysql.createPool({
-      host: process.env.DB_HOST || "localhost",
-      port: Number(process.env.DB_PORT) || 3306,
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "",
-      database: process.env.DB_NAME || "db_tokofootwear",
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
-    });
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
 
     // Test connection
     const conn = await pool.getConnection();
